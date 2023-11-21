017f9a3671fd99d214f0f3e11b08ee6bce806dc3
liocutro.cs <lionel.cutro@gmail.com>
2023-11-21 10:37:06 -0300

    Create 07-Declaraciones-de-cadenas-en-memoria.asm
    add question

---
+++ Trabajo-01/07-Declaraciones-de-cadenas-en-memoria.asm
@@ -0,0 +1,13 @@
+.data
+
+
+cadena:		.byte		"abcde"			
+octeto:		.byte		0xFF
+
+# cuestion 1.14:
+
+# al definir la cadena de caracteres
+# como .byte, el simulador finaliza con
+# errores, ya que no se  úede interpretar el 
+# tipo caracter con un tipo de dato byte, es decir
+# son datos incompatibles de representar
