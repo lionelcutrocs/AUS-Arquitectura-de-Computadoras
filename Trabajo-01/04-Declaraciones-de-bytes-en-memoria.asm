79c7c9ee44cce121edc96d4aa8112ce012b79715
liocutro.cs <lionel.cutro@gmail.com>
2023-11-21 09:29:48 -0300

    Create 04-Declaraciones-de-bytes-en-memoria.asm
    add new question

---
+++ Trabajo-01/04-Declaraciones-de-bytes-en-memoria.asm
@@ -0,0 +1,16 @@
+.data							
+
+
+octeto:		.byte		0x10	
+							
+
+# cuestion 1.7:
+
+# la direccion de memoria se inicializa de buena forma
+# y seria en la direccion 0x10010000
+
+# cuestion 1.8:
+
+# el valor que se almacena en la palabra la cual contiene el byte
+# es 0x10 en hexadecimal y un 16 en decimal	
+
