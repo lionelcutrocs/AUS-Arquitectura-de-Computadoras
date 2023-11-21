89799a61a4c392d240eee01334ab28243ad05b63
liocutro.cs <lionel.cutro@gmail.com>
2023-11-21 11:20:18 -0300

    Create 08-Reserva-de-espacio-en-memoria.asm
    add question

---
+++ Trabajo-01/08-Reserva-de-espacio-en-memoria.asm
@@ -0,0 +1,18 @@
+.data							
+
+
+palabra1:		.word	0x20			# uso word para definir el tamaño en (4 bytes)
+espacio:		.space	8			# con space crea una reserva de espacio de memoria (8 bytes)
+palabra2:		.word	0x30			# uso word para definir el tamaño en (4 bytes)
+
+
+# cuestion 1.15:
+
+# sobre el rango de memoria reservado para espacio
+# se encuentra en 0x10010004 al 0x10010011
+
+# cuestion 1.16:
+
+# bytes reservados en total son 16 bytes,
+# palabra1 y palabra2 4 bytes respectivamente
+# espacio 8 bytes. 
