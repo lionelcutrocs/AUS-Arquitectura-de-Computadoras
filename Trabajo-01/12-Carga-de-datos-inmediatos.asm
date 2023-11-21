75691bc2fb3e57b456bbbbfda53b0c1f49fe94ac
liocutro.cs <lionel.cutro@gmail.com>
2023-11-21 15:15:12 -0300

    Create 12-Carga-de-datos-inmediatos.asm
    new question

---
+++ Trabajo-01/12-Carga-de-datos-inmediatos.asm
@@ -0,0 +1,17 @@
+.text
+
+main:
+ 
+	li $s0, 0x12345678
+
+# cuestion 2.3:
+
+# cuando se ejecuta el programa primero se inicializa
+# el registro $at = 0x12340000 y luego se inicializa el 
+# registro $s0 = 0x12345678 en este orden
+
+# cuestion 2.4:
+
+# cuando el programa fue ejecutado para realizar
+# la pseudoinstruccion li se utilizan dos y son;
+# lui y ori
