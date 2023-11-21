d8477580951788ee2764a7c8dda518c644156fa3
liocutro.cs <lionel.cutro@gmail.com>
2023-11-21 15:05:29 -0300

    Create 11-Carga-de-datos-inmediatos.asm
    add question

---
+++ Trabajo-01/11-Carga-de-datos-inmediatos.asm
@@ -0,0 +1,18 @@
+.text
+		 					
+main:
+ 
+	lui $s0, 0x8690
+
+# cuestion 2.1:
+
+# en el registro $s0 encontramos el valor 0x86900000
+# en la direccion 0x00400000 podemos encontrar esto 0x3c108690
+# y esto es el valor 8690 que se ingresa y donde 3c10 es el opcode de lui 
+
+# cuestion 2.2:
+
+# en la zona de registros $s0 encontramos el valor 0x86900000
+
+# ----------------------------------------------------------------------------------------------------------
+# El tipo de formato que tiene la instruccion maquina  -->  Tipo formato I, ya que es una instruccion de carga inmediata
